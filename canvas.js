eb54f33577e5e79d027ff1c64df91b3cf4067fa7
Damien <abbemad@gmail.com>
2018-05-05 14:48:54 +0200

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -40,35 +40,59 @@ var c = canvas.getContext('2d');
 // random spanw circle + speed / velocity 
 // bouncen wall 
 
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight; 
+// // random spawnm
+// var x = Math.random() * innerWidth;
+// var y = Math.random() * innerHeight; 
 
-var dx = (Math.random() - 0.5) * 50 ;
-var dy = (Math.random() - 0.5) * 50 ;
+// var dx = (Math.random() - 0.5) * 50 ;
+// var dy = (Math.random() - 0.5) * 50 ;
 
-var radius = 30;
+// var radius = 30;
+
+function Circle(x, y, dx, dy, radius){
+
+    this.x = x;
+    this.y = y;
+    
+    this.dx = dx;
+    this.dy = dy;
+
+    this.radius = radius;
+
+    this.draw = function(){
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = 'white';
+        c.stroke();
+    }
+
+    this.update = function (){
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx; 
+        }
+    
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy; 
+        }
+    
+    
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    }
+}
+
+
+var circle = new Circle(200,200, 3, 3, 30);
+circle.draw();
 
 function animation() {
     requestAnimationFrame(animation);
     c.clearRect(0, 0, innerWidth, innerHeight);
 
-    c.beginPath();
-    c.arc(x, y, radius, 0, Math.PI * 2, false);
-    c.strokeStyle = 'white';
-    c.stroke();
+    circle.update();
 
-
-    if (x + radius > innerWidth || x - radius < 0) {
-        dx = -dx; 
-    }
-
-    if (y + radius > innerHeight || y - radius < 0) {
-        dy = -dy; 
-    }
-
-
-    x += dx;
-    y += dy;
 }
 
 animation();
